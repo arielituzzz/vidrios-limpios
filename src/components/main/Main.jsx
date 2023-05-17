17d7a841cd1a78e145c56530e06a97ef6329b528
Ariel Suarez <suarezariel86@gmail.com>
2023-05-17 14:51:01 -0300

    inicializando repo

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./home/Home";
+import "./_main.scss";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <Home />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/main/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./_home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <p className="home">home</p>
+      <p className="darkBlue">Hormigon Industrial</p>
+      <p className="vidrios">vidrios limpios</p>
+      <p className="gris">Equipos de profesionales</p>
+      <p className="blueLightLight">LIMPIEZA EN ALTURA</p>
+    </div>
+  );
+};
+
+export default Home;
