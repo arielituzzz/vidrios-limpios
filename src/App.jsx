181ac0379295d03973a0eb2b2f7002953ac77975
Ariel Suarez <suarezariel86@gmail.com>
2023-06-01 11:08:27 -0300

    Se anadio referencias para ingresar al top de las rutas y se corrigio alineacion del titulo de la seccion distribuidora ofial thames

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { HashRouter as Router, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
--- src/components/main/thamesDistributor/ThamesDistributor.jsx
+++ src/components/main/thamesDistributor/ThamesDistributor.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./_thamesDistributor.scss";
+import Top from "../top/Top";
 import HeaderAux from "../../headerAux/HeaderAux";
-import imgBackground from "../../../assets/imgs/distribuidora-thames.png";
 import PresentationThames from "./presentation/PresentationThames";
+import imgBackground from "../../../assets/imgs/distribuidora-thames.png";
 
 const ThamesDistributor = () => {
+  const location = useLocation();
+  useEffect(() => {
+    const { hash } = location;
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
   return (
     <div>
+      <Top />
       <HeaderAux
-        title="Distribuidora Oficial 'CASA THAMES'"
+        title="Distribuidor Oficial 'CASA THAMES'"
         background={imgBackground}
       />
       <PresentationThames />
